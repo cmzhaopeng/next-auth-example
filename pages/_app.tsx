794ca8e240a16f292c8e70ac34c7f6fcf6e446ee
unknown <cmzhaopeng@139.com>
2022-12-31 15:51:44 +0800

    add publish and delete function

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,30 @@
-import '../styles/globals.css'
+
+import { SessionProvider } from 'next-auth/react';
+import { AppProps } from 'next/app';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+export default App;
+
+
+
+/*import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import {SessionProvider} from 'next-auth/react'
 
 
-export default function App({ 
-  Component, 
-  pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
- 
-    <SessionProvider session={pageProps.session}>
-  
+ <SessionProvider session={pageProps.session}>
   <Component {...pageProps} />
-
-  </SessionProvider>
-
-  )
+</SessionProvider>
+)
 }
+*/
