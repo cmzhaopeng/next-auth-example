20e738793f257fb43df848ea5b1006dc84925ff0
unknown <cmzhaopeng@139.com>
2023-01-05 00:05:50 +0800

    add a row in x-data-grid

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -5,22 +5,25 @@ import Layout from "../components/Layout";
 import { GetServerSideProps } from "next";
 import { useSession, getSession } from "next-auth/react";
 import prisma from "../lib/prisma";
-import { DataGrid, GridRowsProp, GridColDef} from '@mui/x-data-grid'
+import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import cuid from "cuid";
 
 const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'name', headerName: 'Name', width: 130 },
-    { field: 'email', headerName: 'Email', width: 130 },
-    { field: 'image', headerName: 'Image', width: 130 },
-    ];
-
+  { field: "id", headerName: "ID", width: 270 },
+  { field: "name", headerName: "Name", width: 130 },
+  { field: "email", headerName: "Email", width: 130 },
+  { field: "image", headerName: "Image", width: 130 },
+];
 
 type UserProps = {
-    id: string;
-    name: string;
-    email: string;
-    image: string;
-  };
+  id: string;
+  name: string;
+  email: string;
+  image: string;
+};
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const session = await getSession({ req });
@@ -29,13 +32,12 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     return { props: { users: [] } };
   }
 
-  const isAdmin= session?.user?.email === process.env.NEXT_PUBLIC_ADMIN_EMAIL
+  const isAdmin = session?.user?.email === process.env.NEXT_PUBLIC_ADMIN_EMAIL;
   if (!isAdmin) {
     res.statusCode = 403;
     return { props: { users: [] } };
   }
-  const users = await prisma.user.findMany({
-  });
+  const users = await prisma.user.findMany({});
   return {
     props: { users },
   };
@@ -45,14 +47,27 @@ type Props = {
   users: UserProps[];
 };
 
-const Admin: React.FC<Props> = ( props ) => {
+const Admin: React.FC<Props> = (props) => {
   const { data: session } = useSession();
-  const [selectedRowData, setSelectedRowData] = React.useState([] as UserProps[]);
+  const [selectedRowData, setSelectedRowData] = React.useState(
+    [] as UserProps[]
+  );
+  const [rows, setRows] = React.useState(props.users);
+
+  const handleAddRow = async () => {
+    const uid= cuid();
+    const newRow = {
+      id: uid,
+      name: "New User",
+      email: "New Email",
+      image: "New Image",
+    };
+    setRows((prevRows) => [...prevRows, newRow]);
+  };
 
   const submitData = async () => {
-    
     try {
-    /*  const body = { title, content };
+      /*  const body = { title, content };
       await fetch("/api/post", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -60,13 +75,11 @@ const Admin: React.FC<Props> = ( props ) => {
       });
       await Router.push("/drafts");
       */
-     await console.log(selectedRowData);
-
+      await console.log(selectedRowData);
     } catch (error) {
       console.error(error);
     }
   };
-
 
   if (!session) {
     return (
@@ -81,7 +94,7 @@ const Admin: React.FC<Props> = ( props ) => {
       <div className="page">
         <h1>Admin</h1>
         <main>
-        {/*  
+          {/*  
           {props.users.map((user) => (
             <div key={user.id} className="user">
               <h2>{user.name}</h2>
@@ -90,18 +103,28 @@ const Admin: React.FC<Props> = ( props ) => {
           ))}
           */}
 
-<div style={{ height: 300, width: '100%' }}>
-      <DataGrid rows={props.users} columns={columns} onSelectionModelChange={(ids) => {
-  const selectedIDs = new Set(ids);
-  const selectedRowData1 = props.users.filter((row) =>  selectedIDs.has(row.id.toString()));
-  setSelectedRowData(selectedRowData1);
-  //console.log(selectedRowData);
-  submitData();
-
-}}
-/>
-    </div>
-
+          <Box sx={{ width: "100%" }}>
+            <Stack direction="row" spacing={2}>
+              <Button size="small"  onClick={handleAddRow}>
+                Add a Row
+              </Button>
+              </Stack>
+              <Box sx={{ height: 400, mt: 1 }}>
+              <DataGrid
+                rows={rows}
+                columns={columns}
+                onSelectionModelChange={(ids) => {
+                  const selectedIDs = new Set(ids);
+                  const selectedRowData1 = rows.filter((row) =>
+                    selectedIDs.has(row.id.toString())
+                  );
+                  setSelectedRowData(selectedRowData1);
+                  //console.log(selectedRowData);
+                  submitData();
+                }}
+              />
+              </Box>
+          </Box>
         </main>
       </div>
       <style jsx>{`
